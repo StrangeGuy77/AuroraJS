02977433a30d63471614f1e710a2b05f162e33d8
Jhonatan Restrepo <53962640+StrangeGuy77@users.noreply.github.com>
2019-08-18 15:24:15 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const { database } = require('./keys');
-// Destructura el objeto para acceder únicamente a él.
 
 mongoose.connect(database.URI, {useNewUrlParser: true}).then((value) => {
     console.log(`Conectado a la base de datos`)

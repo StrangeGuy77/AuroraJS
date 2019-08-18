1388b5d993c489b2eb11af5f42ae81c287385c83
Jhonatan Restrepo <53962640+StrangeGuy77@users.noreply.github.com>
2019-08-18 15:20:52 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -3,11 +3,11 @@ module.exports = {
         URI: 'mongodb://localhost/socialnetwork'
     },
     Contactmailer: {
-        user: "strangeguy94@gmail.com",
-        pass: "losdelsur123"
+        user: "test@user.com",
+        pass: "testpass"
     },
     Api: {
-        client_id: '0a17b0f0-4ec4-4530-af1a-f460a6b66716',
-        client_secret: 'E8sE6jD1oM0sU0fT6cJ5dF0hE4vO6kB6fA5eF8wR8kW3pV4eD4'
+        client_id: 'test@client',
+        client_secret: 'test@secret'
     }
 };

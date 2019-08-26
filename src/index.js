cf43f83164ca5d415fed64a1dca25f9a6b3794bb
Jhonatan Restrepo <53962640+StrangeGuy77@users.noreply.github.com>
2019-08-26 05:07:39 -0500

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const config = require('./server/config');
-require(`./database`);
-const app = config(express());
-
-app.listen(app.get(`port`), () => {
-    console.log(`Server on port: ${app.get('port')}`);
-});

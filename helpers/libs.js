38d3e6ad70bbc8ed4691cb17380799191606de4e
Jhonatan Restrepo <53962640+StrangeGuy77@users.noreply.github.com>
2019-10-01 18:36:33 +0200

    Helpers updated

--- helpers/libs.js
+++ helpers/libs.js
@@ -2,7 +2,7 @@ const helper = {};
 
 helper.randomName = () => {
   // '/()%' keywords send error whenever they're in the name of any image.
-  const possible = "abcdefghijklmnopqrstuvwxyz$&0123456789";
+  const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
   let randomName = 0;
   for (let index = 0; index < 6; index++) {
     randomName += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -49,6 +49,16 @@ helper.toBoolean = string => {
       return false;
       break;
   }
+};
+
+helper.checkBuy = (collection, object) => {
+  for (let index = 0; index < collection.length; index++) {
+    if (collection[index].id === object) {
+      index = collection.length + 1;
+      return true;
+    }
+  }
+  return false;
 };
 
 module.exports = helper;

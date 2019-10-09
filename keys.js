671be4a5bf71355a4b06077706a9e33ae91f7f44
Jhonatan Restrepo <53962640+StrangeGuy77@users.noreply.github.com>
2019-10-08 20:54:11 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -13,8 +13,8 @@ module.exports = {
     }
   },
   Api: {
-    client_id: "0a17b0f0-4ec4-4530-af1a-f460a6b66716",
-    client_secret: "E8sE6jD1oM0sU0fT6cJ5dF0hE4vO6kB6fA5eF8wR8kW3pV4eD4"
+    client_id: "client_id",
+    client_secret: "client_secret"
   },
   DefaultLocale: {
     defaultLanguage: "en",
